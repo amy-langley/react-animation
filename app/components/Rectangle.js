aa5dc5b23601059259a74f57cf549fbdef8b6028
Amy Boyer <amy.r.boyer@gmail.com>
2015-11-22 23:58:04 -0600

    a few tweaks

--- app/components/Rectangle.js
+++ app/components/Rectangle.js
@@ -20,10 +20,10 @@ export default class Rectangle extends React.Component {
   render() {
     var when = new Date().getTime()
     var offset = (when/100)%100
-    var two = 25+25*Math.cos(when/200)
+    var two = 30+30*Math.cos(when/200)
 	var color='hsla(150,80%,'+(20+40*(1+Math.sin(when/2000)))+'%,'+(1+Math.cos(when/300)/2.0)+')'
     return (
-		<rect height={this.scaleY(30)} width={this.scaleX(30)} x={1.3*this.scaleX(offset-15)} y={1.3*this.scaleY(two-15)} fill={color}/>
+		<rect height={this.scaleY(30)} width={this.scaleX(30)} x={Math.floor(1.3*this.scaleX(offset-15))} y={Math.floor(1.3*this.scaleY(two-15))} fill={color}/>
     );
   }
 }
